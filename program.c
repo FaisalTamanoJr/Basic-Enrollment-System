645748c28f5b85b9a71d9e6b47dc0ee05b0240cb
Faisal Richard Jr <faisalrichardjr@gmail.com>
2023-04-21 11:48:05 +0800

    added multiple functions and other things

---
+++ program.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+char courses[10][10] = {};
+char sections[2][3] = {};
+
+// Login function
+void login()
+{
+}
+
+// Display profile function
+void displayStudentProfile()
+{
+}
+
+// Course enrollment function
+void courseEnrollment()
+{
+}
+
+// Fees function
+void fees()
+{
+}
+
+// Main function
+void main()
+{
+}
+
