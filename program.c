d359d072337e86ee7df15c9cb2eaae5d96dfb7f9
Faisal Richard Jr <faisalrichardjr@gmail.com>
2023-04-21 13:38:02 +0800

    major changes

--- program.c
+++ program.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
-char courses[10][10] = {};
-char sections[2][3] = {};
+// Constants
+#define NumberOfCourses 10
+#define NumberOfUsers 4
+#define MaxCourses 12
+
+// User Arrays
+char usernames[NumberOfUsers][20] = {};
+char passwords[NumberOfUsers][20] = {};
+char user_details[NumberOfUsers][8][20] = {};
+char user_courses[NumberOfUsers][MaxCourses][10];
+
+// Course Arrays
+char courses[NumberOfCourses][10] = {};
+char course_details[NumberOfCourses][5][10] = {}; // [course index][type, section, time, day, room]
+int course_units[NumberOfCourses] = {};
 
 // Login module
-void login()
+bool login(char *username, char *password)
 {
 }
 
 // Display profile module
-void display_student_profile()
+void display_student_profile(char *username)
 {
 }
 
